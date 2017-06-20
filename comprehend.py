bc2b1268009d7c505e57dccb3eb0a343b1225497
Andrew Scott <ascott@hmc.edu>
2017-06-20 14:43:12 -0700

    added a sample interface for the question answering

---
+++ comprehend.py
@@ -0,0 +1,12 @@
+
+
+class Comprehender:
+
+    def __init__(self, text):
+        self.text = text
+
+    def answer_question(self, question):
+        """
+        Answers the question with a snippet of text from within self.text.
+        """
+        pass
